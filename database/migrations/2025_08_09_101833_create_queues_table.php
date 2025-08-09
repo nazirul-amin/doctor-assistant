cae793a0ecce93eae9ad31c28c603cfcb3137a1a
nazirulamin <nazirul.retes@gmail.com>
2025-08-09 19:08:34 +0800

    update migrations

--- database/migrations/2025_08_09_101833_create_queues_table.php
+++ database/migrations/2025_08_09_101833_create_queues_table.php
@@ -13,6 +13,19 @@ return new class extends Migration
     {
         Schema::create('queues', function (Blueprint $table) {
             $table->id();
+            $table->string('name')  ;
+            $table->unsignedInteger('age')  ;
+            $table->enum('gender', ['male', 'female', 'other']);
+            $table->enum('status', ['waiting', 'in_progress', 'completed', 'cancelled'])->default('waiting');
+            $table->unsignedBigInteger('patient_id')->nullable();
+            $table->unsignedBigInteger('consultation_id')->nullable();
+            $table->unsignedBigInteger('processed_by')->nullable();
+            $table->timestamp('processed_at')->nullable();
+            $table->integer('queue_number');
+
+            $table->foreign('patient_id')->references('id')->on('patients')->onDelete('cascade');
+            $table->foreign('consultation_id')->references('id')->on('consultations')->onDelete('cascade');
+            $table->foreign('processed_by')->references('id')->on('users')->onDelete('set null');
             $table->timestamps();
         });
     }
